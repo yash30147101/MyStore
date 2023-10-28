00159a94b04f3e6bd245324faa5f770d69a0ef30
yash30147101 <yashtrada1@gmail.com>
2023-10-28 13:45:06 -0300

    Completing frontend pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
@@ -16,4 +17,4 @@ const App = () => {
   );
 };
 
-export default App
+export default App;
